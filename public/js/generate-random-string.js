2ae4569db03047b57ab1264ea9f361d50f448c86
Justin Zelinsky <jpzelinsky@gmail.com>
2020-11-22 18:54:29 -0500

    Redo random string generation

--- public/js/generate-random-string.js
+++ public/js/generate-random-string.js
@@ -1,5 +1,15 @@
+// Found this function here: https://stackoverflow.com/a/27747377
+
+function dec2hex (dec) {
+  return dec < 10
+    ? '0' + String(dec)
+    : dec.toString(16);
+}
+
 function generateRandomString () {
-  return Math.random().toString(36).slice(2);
+  const arr = new Uint8Array(10);
+  window.crypto.getRandomValues(arr);
+  return Array.from(arr, dec2hex).join('');
 }
 
 export { generateRandomString };
